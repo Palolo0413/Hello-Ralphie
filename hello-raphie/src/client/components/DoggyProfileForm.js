885d893d0ada263e8a42b4585185a50933d254e1
Yang Gu <yg2349@gmail.com>
2021-02-23 16:30:34 -0500

    adjusted layout

--- hello-raphie/src/client/components/DoggyProfileForm.js
+++ hello-raphie/src/client/components/DoggyProfileForm.js
@@ -4,7 +4,10 @@ import {
   InputLabel,
   Input,
   Button,
-  TextField
+  TextField,
+  Grid,
+  Box,
+  Typography
 } from '@material-ui/core'
 import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles'
 
@@ -23,47 +26,85 @@ let theme = createMuiTheme({
 });
 theme = responsiveFontSizes(theme)
 
+
 class DoggyProfileForm extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
+      <Grid container spacing={16} color='primary'>
+      <Grid item xs='12' sm='8' display='inline' coor='primary' >
       <div
         style={{
           display: 'flex',
           justifyContent: 'center',
-          margin: 20,
-          padding: 20,
+          margin: 10,
+          padding: 10,
           color:'#B69594',
           backgroundColor:'#FCEFD5'
         }}
       >
-        <form style={{width: '33%', color:'#B69594'}} >
+        <form style={{width: '80%', height:'99%', color:'#B69594'}} >
           <h1 color="#B69594">Your Doggy Profile</h1>
 
-          <FormControl margin="normal" fullWidth color="primary">
+          <FormControl margin="normal" fullWidth color="primary" focused='true'>
             <InputLabel htmlFor="name" color="primary">Name</InputLabel>
             <Input id="name" type="text" color="primary" />
           </FormControl>
 
-          <FormControl margin="normal" fullWidth color="primary">
+          <FormControl margin="normal" fullWidth color="primary" focused='true'>
             <InputLabel htmlFor="breed" color="primary">Breed</InputLabel>
             <Input id="breed" type="text" color="primary"/>
           </FormControl>
 
-          <FormControl margin="normal" fullWidth>
-            <TextField
+          <FormControl margin="normal" fullWidth color="primary" focused='true' variant='outlined'>
+          
+          <TextField
+                variant='outlined'
+                variant='filled'
+                focused='true'
+                backgroundColor='primary'
                 color="primary"
                 id="birthday"
                 label="Birthday"
                 type="date"
-                InputLabelProps={{shrink: true,}}/>
-          </FormControl>
-
-          <Button variant="contained" color="secondary" size="medium">
+                InputLabelProps={{shrink: true,}}
+                InputProps={{color:"primary", backgroundColor:'primary' }}/>
+      </FormControl>
+      <Box 
+        display="right" 
+        alignItems="right"
+        justifyContent="center"
+        bgcolor="primary"
+        paddingTop={4}
+      >
+          <Button variant="contained" color="secondary" size="large" textAlign="center" >
             Next
           </Button>
+          </Box>
         </form>
       </div>
+      </Grid>
+      <Grid xs='12' sm='4' color='primary'>
+      <Grid container direction="column" justify="center" alignItems="center">
+      <Grid>
+      <img alt=' ' src='DoggoBig.png' display='inline' height='150px' padding={20}/>
+      </Grid>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid>
+      <Typography variant="h4">
+       FIDO
+      </Typography >
+      </Grid>
+      <Typography variant="h5">
+       Golden Lab
+      </Typography>
+      <Typography variant="h5">
+       Five Years Old
+      </Typography>
+      </Grid>
+      </Grid>
+      </Grid>
+      </Grid>
       </ThemeProvider>
     )
   }
